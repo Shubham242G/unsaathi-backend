39f54dbaf96bdc023305f3b867614d3e770b8a0f
Shubham242G <allhailvicky242@@gmail.com>
2026-01-11 10:53:14 +0530

    mongo_uri name match

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json({ limit: '50mb' }));        // ✅ Base64 images
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB connection
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err));
 
